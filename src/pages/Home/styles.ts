8430ee6b3509a23c16a09d7389d6f046debd69cd
Rafael Kepler <rafael.kepler@hotmail.com>
2024-05-22 16:24:08 -0300

    styling of the home page done

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -51,6 +51,9 @@ export const BaseInput = styled.input`
 
 export const TaskInput = styled(BaseInput)`
   flex: 1;
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
 `
 export const MinutesAmountInput = styled(BaseInput)`
   width: 4rem;
